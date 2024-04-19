04bd425f765e643f01bf40981dacff3958b8285e
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-04-18 23:27:10 -0300

    insert implementation - Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,16 +10,21 @@ public class Program2 {
   
   public static void main(String[] args) {
 	
-	DepartmentDao departamentDao = DaoFactory.createDepartmentDao();
+	DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 	
 	System.out.println("==================== TEST 1: department findById ====================");
-	Department dep = departamentDao.findById(1);
+	Department dep = departmentDao.findById(1);
 	System.out.println(dep);
 	
 	System.out.println("\n==================== TEST 2: department findAll ====================");
-	List<Department> list = departamentDao.findAll();
+	List<Department> list = departmentDao.findAll();
 	for (Department obj : list) {
 	  System.out.println(obj);
 	}
+	
+	System.out.println("\n==================== TEST 3: department insert ====================");
+	Department newDepartment = new Department(null, "Music");
+	departmentDao.insert(newDepartment);
+	System.out.println("Inserted! New id = " + newDepartment.getId());
   }
 }
