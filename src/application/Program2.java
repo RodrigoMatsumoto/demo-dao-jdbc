8859671bae201b3c26798a04b34044ebbb493713
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-04-18 23:35:04 -0300

    delete implementation - Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,7 @@ public class Program2 {
   
   public static void main(String[] args) {
 	
+	Scanner sc = new Scanner(System.in);
 	DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 	
 	System.out.println("==================== TEST 1: department findById ====================");
@@ -32,5 +34,13 @@ public class Program2 {
 	dep.setName("Food");
 	departmentDao.update(dep);
 	System.out.println("Update completed");
+	
+	System.out.println("\n==================== TEST 6: department delete ====================");
+	System.out.println("Enter id for delete test: ");
+	int id = sc.nextInt();
+	departmentDao.deleteById(id);
+	System.out.println("Delete completed");
+	
+	sc.close();
   }
 }
